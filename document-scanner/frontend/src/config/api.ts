2966715643178f2f2844c0a59649e6f26d8bda62
Shaurya Dabral <shaurya.daral.ye7c@Qyupe.com>
2026-02-19 14:38:37 +0530

    update

--- document-scanner/frontend/src/config/api.ts
+++ document-scanner/frontend/src/config/api.ts
@@ -1,5 +1,4 @@
-const envBaseUrl = import.meta.env.VITE_API_BASE_URL;
-
+const envBaseUrl = "https://doc-scanner-app-for-eazypetition.onrender.com";//hardcoded for now, after sometime add env variable to the frontend deployment.
 let resolvedBaseUrl = envBaseUrl;
 
 if (!resolvedBaseUrl || resolvedBaseUrl.length === 0) {
